a1703acd7ec6c6cc2836bcbad1cde90f753a13c1
ew513 <eatonwang@outlook.com>
2023-07-20 12:19:56 +0800

    Site updated: 2023-07-20 12:19:55

--- js/wheather.js
+++ js/wheather.js
@@ -1,1 +1,1 @@
-document.writeln("<iframe scrolling=\'no\' src=\'https://tianqiapi.com/api.php?style=ye&skin=pitaya\' frameborder=\'0\' width=\'100\' height=\'40\' allowtransparency=\'true\'></iframe>");
+document.writeln("<iframe scrolling=\'no\' src=\'https://tianqiapi.com/api.php?style=ye&skin=pitaya\' frameborder=\'0\' width=\'120\' height=\'40\' allowtransparency=\'true\'></iframe>");
