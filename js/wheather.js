def901de41050b01fb41ea1fd302a0fbf765c8f6
ew513 <eatonwang@outlook.com>
2023-07-20 12:10:05 +0800

    Site updated: 2023-07-20 12:10:05

--- js/wheather.js
+++ js/wheather.js
@@ -1,1 +1,1 @@
-document.writeln("<iframe scrolling=\'no\' src=\'https://tianqiapi.com/api.php?style=ye&skin=pitaya\' frameborder=\'0\' width=\'30\' height=\'40\' allowtransparency=\'true\'></iframe>");
+document.writeln("<iframe scrolling=\'no\' src=\'https://tianqiapi.com/api.php?style=ye&skin=pitaya\' frameborder=\'0\' width=\'50\' height=\'40\' allowtransparency=\'true\'></iframe>");
