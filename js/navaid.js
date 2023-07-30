9e35462f4e87cde46af1dbcdbb9c727ce5398db2
NavaidX <eatonwang123@gmail.com>
2023-07-31 02:07:03 +0800

    Site updated: 2023-07-31 02:07:03

---
+++ js/navaid.js
@@ -0,0 +1,9 @@
+{
+    var etxt = '欢迎访问『Navaid · 柰蔚德』';
+    console.log('%c'+etxt,'color:green;');
+
+    var str = '主站地址: https://www.navaid.rf.gd \n';
+    str += '镜像站地址: https://navaid.gitee.io \n';
+    str += '注: 镜像站目前主要用于测试，后续会定期开放。';
+    console.log(str);
+}
